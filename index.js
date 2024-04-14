bf48f79bd7e499dce23bc91e5ccc243feb1afc74
brian kanari <briangitui26@gmail.com>
2024-04-14 14:07:42 +0300

    creates loop

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards( stringnames, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < stringnames.length; i++ ) {
+      thankYouCards.push( `Thank you, ${stringnames[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log(startingNumber)
+       startingNumber -=1
+    }
+    console.log(startingNumber);
+  }
